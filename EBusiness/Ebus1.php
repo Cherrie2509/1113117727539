9a0812fd209395288fcc50dd0ae50ab8b3b02022
Cherrie Ding <117727539@umail.ucc.ie>
2018-02-19 12:32:27 +0000

    gg

--- EBusiness/Ebus1.php
+++ EBusiness/Ebus1.php
@@ -3,54 +3,54 @@
     <head>
         <title>Select Product</title>
         
-        <!--jQuery-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src="cost_calc.js"></script>
-
-    </head>
-
-        <h4>Select a Product</h4>
-        
-        <br/>
-        
-        <form method="POST" action="ebus2.php"></form>
-        
-        <label for="Salesforce">
-            <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
-            Salesforce @ $100
-        </label>
-
-        <br/>
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+       <script type="text/javascript" src="cost_calc.js"></script>
        
-       <label for="aws">
+    </head>
+    
+    <body>
+            <h4>Select a Product</h4>
+            
+            <br/>
+            
+            <form method="post" action="ebus2".pho>
+            
+            <label for ="salesforce">
+            <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+            salesforce @ $100
+            </label>
+            
+            <br/>
+            
+            <label for ="aws">
             <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
             AWS @ $300
-        </label>
-        
-        <br/>
-        <br/>
-        
-        <label for="Subtotal">
-            Sub Total
-            <input type="Text" id="subtotal" value="0.00" readonly/>
-        </label>
-        
-        <br/>
-        
-        <label for="Total">
-            Total
-        <input type="Text" id="subtotal" value="0.00" readonly/>
-        </label>
-        
-        <br/>
-        
-        <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
-        
-        </form>
-        
-        <br/>
-        <button onClick="calcSub()">Calculate Cost</button>
-        <a role="button" href="eBus1.php">ClearChoice</a>
-        
+            </label>   
+            
+            <br/>
+            <br/>
+            
+            <label for="subtotal">
+                Sub Total
+             <input type="text" id="subtotal" value="0.00" readonly/>
+            </label>     
+                
+            <br/>
+            
+            <label for="total">
+                total
+                <input type="text" id="total" value=0.00 readonly/>
+            </label>
+    
+            <br/>
+            
+            <button type="submit" id="btnproceed" disabled>add to shopping cart</button>
+            
+            </form>
+            
+            <br/>
+            <button onclick="calcSub()">Calculate Cost</button>
+            <a role="button" href="ebus1.php">clear choice</a>
+            
     </body>
-    </head>
+</html>
